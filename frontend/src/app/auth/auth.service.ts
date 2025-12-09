9eb385d4a4f7970800a015afeef52738432e0e43
Dmitriy Okhrimenko <d.okhrimenko@cbsystematics.com>
2025-12-09 13:11:15 +0200

    auth service fix

--- frontend/src/app/auth/auth.service.ts
+++ frontend/src/app/auth/auth.service.ts
@@ -4,7 +4,6 @@ import { tap } from 'rxjs/operators';
 import type { AuthUser } from '../model/auth-user';
 import type { AuthResponse } from '../model/auth-response';
 import { environment } from '../../environments/environment';
-import { roles } from "../model/roles";
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
